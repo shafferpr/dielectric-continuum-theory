2ed5cdc94db8dd99bef44a4748c4e39174f9a1ed
Patrick Shaffer <shafferpr@gmail.com>
2012-02-05 19:59:00 -0800

    beginning to work on spherharm.c

--- spherharm.c
+++ spherharm.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
-#include <gsl/gsl_sf_bessel.h>
+#include <gsl/gsl_sf_legendre.h>
+#include <complex.h>
+#include <math.h>
+#include <stdlib.h>
+double YLMcomponents[20][40]={0};
+void projectontoYLM();
+
 
 int main(void){
   double x = 5.0;
-  double y = gsl_sf_bessel_J0 (x);
+  double y = gsl_sf_legendre_sphPlm (1, 1, .5);
   printf("J0(%f) = %.18f\n", x, y);
   return 0;
+}
+
+
+void projectontoYLM(){
+
 }
